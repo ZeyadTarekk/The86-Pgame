94bcb830fbd5266e6bbd7be4d1b5f24e75a3d88b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-12-12 20:54:35 +0200

    Destination offsetSetter.inc

--- Destination/offset.asm
+++ Destination/offset.asm
@@ -8,29 +8,6 @@ registersOffsets dw 16 dup(00)
 .code
 
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Written by : - Abdelrahman Hamza  12-12-2021
-; parameters : - two arrays 1) input array 2) output array                            ;;
-; return     : - fill output array with offsets of input array                        ;;
-;A macro that fills registersOffsets array with offsets of registers array            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-offsetSetter MACRO InputRegisters, InputRegistersOffsets                              ;;
-    LOCAL offsetLoop                                                                  ;;
-    ; loop 16 times => number of registers                                            ;;
-    mov cx,30                                                                         ;;
-     ; Loop start                                                                     ;;
-     offsetLoop:                                                                      ;;
-        mov bx,cx                                                                     ;;
-        mov ax,offset InputRegisters                                                  ;;
-        add ax,cx                                                                     ;;
-        mov InputRegistersOffsets[bx],ax                                              ;;
-        dec cx                                                                        ;;
-     loop offsetLoop                                                                  ;;
-ENDM                                                                                  ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 main proc far
 
     mov ax,@data
