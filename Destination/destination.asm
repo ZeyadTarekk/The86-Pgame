d13cb341c6df911f75e1601b73b1763a95faa41b
Ziad Sherif Muhammed Ibrahim <78238570+ZiadSheriif@users.noreply.github.com>
2021-12-13 00:26:36 +0200

    Searching

--- Destination/destination.asm
+++ Destination/destination.asm
@@ -1,21 +1,26 @@
+include lowercase.inc
+include offsetSetter.inc
+include searchOffset.inc
 .model small
 .stack 64
 .data
 
-registers        dw 'ax','bx','cx','dx','si','di','bp','sp','ah','al','bh','bl','ch','cl','dh','dl'
-registersOffsets dw 16 dup(00)
-
+    registers        dw 'ax','bx','cx','dx','si','di','bp','sp','ah','al','bh','bl','ch','cl','dh','dl'
+    registersOffsets dw 16 dup(00)
+    Str              dw 'DXDXDXDXDXDXDXDXDXDXDXDXDXDXDXDXDX$'
 .code
 
 
 main proc far
 
-    mov ax,@data
-    mov ds,ax
-    mov es,ax
-    offsetSetter registers,registersOffsets
-    mov bx,registersOffsets[2]
-    mov [bx],12ffh
-    hlt
+         mov          ax,@data
+         mov          ds,ax
+         mov          es,ax
+         lowercase    Str
+         offsetSetter registers,registersOffsets
+         search       Str,registersOffsets,registers
+         mov          bx,registersOffsets[2]
+         mov          [bx],12ffh
+         hlt
 main endp
 end main
